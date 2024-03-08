17dae23d5cd5815b5ba4415d82d3133cf5c225f5
SoraPay <laichuks@yandex.ru>
2024-03-08 20:47:59 +0300

    complete 1 task

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int m = InputDigit("Введите минимальное число: ");
+    int n = InputDigit("Введите максимальное число: ");
+    PrintAllNaturalDigit(m, n);
+}
+
+void PrintAllNaturalDigit(int m, int n)
+{
+    if (m <= n)
+    {
+        System.Console.Write(m + " ");
+        PrintAllNaturalDigit(m + 1, n);
+    }
+}
+
+int InputDigit(string textPrint)
+{
+    System.Console.Write(textPrint);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
